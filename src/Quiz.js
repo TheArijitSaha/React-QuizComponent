e085782607fa6280b01e5012caf8f014523bc006
Arijit Saha <arijitbiley@gmail.com>
2020-05-29 15:56:59 +0530

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,12 +17,17 @@ class Quiz extends Component {
 			quiz_position: state.quiz_position + 1
 		}));
 	}
+	handleResetClick() {
+		this.setState((state, props) => ({
+			quiz_position: 1
+		}));
+	}
 	render() {
 		const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length); 
 		return (
 			<div>
 				{(isQuizEnd
-					? <QuizEnd />
+					? <QuizEnd resetClickHandler = { this.handleResetClick.bind(this) } />
 					: <QuizQuestion showNextQuestionHandler = { this.showNextQuestion.bind(this) } quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} />)}
 			</div>
 		);
